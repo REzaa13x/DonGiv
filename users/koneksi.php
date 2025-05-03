66ff818d25544061f37b327e03de0b595164f2a6
aninda013 <anindaaairfa@gmail.com>
2025-05-03 17:24:08 +0700

    menambahkan php

--- users/koneksi.php
+++ users/koneksi.php
@@ -6,6 +6,9 @@ $db   = "tubes_webpro"; // database baru yang kamu pilih
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
+// Tambahkan ini agar $conn juga tersedia
+$conn = $koneksi;
+
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
